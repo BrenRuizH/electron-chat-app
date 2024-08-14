a9be74ad6c47f4164f4689f59fd8d5b96e6aa062
Brenda Ruiz <ruizhernandezbrendac@gmail.com>
2024-08-13 20:40:16 -0600

    Activar la integracion con Node

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const { app, BrowserWindow } = require('electron');
 function createWindow() {
     let win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
     });
 
     win.loadFile("index.html");
-
-    win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
