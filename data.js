6f074b9b43c0c75d2f5ad324553aef1932636e9f
Brenda Ruiz <ruizhernandezbrendac@gmail.com>
2024-08-13 21:11:10 -0600

    Inicializar listados

---
+++ data.js
@@ -0,0 +1,68 @@
+const contacts = [
+    {
+        'name': 'Devon Hudson',
+        'image': 'https://randomuser.me/api/portraits/men/81.jpg',
+        'last_chat': [
+            {
+                'date': '07:35 PM',
+                'message': 'Nulla cillum esse pariatur eu dolor velit consectetur.'
+            }
+        ]
+    },
+    {
+        'name': 'Lily Reid',
+        'image': 'https://randomuser.me/api/portraits/women/56.jpg',
+        'last_chat': [
+            {
+                'date': '06:51 PM',
+                'message': 'Consequat amet dolore Lorem et veniam non ullamco nostrud quis.'
+            }
+        ]
+    },
+    {
+        'name': 'Aubrey Chambers',
+        'image': 'https://randomuser.me/api/portraits/women/95.jpg',
+        'last_chat': [
+            {
+                'date': '02:14 PM',
+            'message': 'Eu amet tempor est ad nisi et quis quis tempor cillum ut eu.'
+            }
+        ]
+    },
+    {
+        'name': 'Marjorie Morgan',
+        'image': 'https://randomuser.me/api/portraits/women/0.jpg',
+        'last_chat': [
+            {
+                'date': '11:08 AM',
+                'message': 'Ut ut ullamco magna enim laborum commodo incididunt anim magna reprehenderit et tempor proident non.'
+            }
+        ]
+    }
+];
+
+const chats = [
+    {
+        'user': {
+            'name': 'Devon Hudson',
+            'image': 'https://randomuser.me/api/portraits/men/81.jpg',
+        },
+        'chat': {
+            'date': '07:35 PM',
+            'message': 'Reprehenderit non aliqua id in. Incididunt do laboris dolor cupidatat do sint pariatur enim cupidatat labore est. Commodo voluptate amet duis minim occaecat. Eiusmod velit velit ut consectetur commodo reprehenderit anim duis id eiusmod minim nisi.'
+        }
+    },
+    {
+        'user': {
+            'name': 'Lillie Warren',
+            'image': 'https://randomuser.me/api/portraits/women/91.jpg',
+        },
+        'chat': {
+            'date': '07:36 PM',
+            'message': 'Ipsum aliquip laborum ad exercitation quis pariatur ullamco. Quis aliquip ullamco non irure cupidatat consequat laboris ipsum fugiat cillum. Lorem id consectetur Lorem excepteur ut fugiat veniam adipisicing cillum magna.'
+        }
+    }
+];
+
+module.exports.contacts = contacts;
+module.exports.chats = chats;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 
+const { chats, contacts } = require('./data');
+
 function createWindow() {
     let win = new BrowserWindow({
         width: 800,
@@ -13,8 +15,11 @@ function createWindow() {
     win.loadFile("index.html");
 
     win.webContents.on('did-finish-load', () => {
-        console.log('did-finish-load');
-        win.webContents.send('data-from-server', { 'key':'value' });
+        //console.log('did-finish-load');
+        //win.webContents.send('data-from-server', { 'key':'value' });
+        //win.webContents.send('pr-init', [chats, contacts]);
+        win.webContents.send('pr-chats', chats);
+        win.webContents.send('pr-contacts', contacts);
     });
 
     ipcMain.on('data-from-web', (event, data) => {
